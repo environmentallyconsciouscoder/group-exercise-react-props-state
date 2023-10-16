fbcea66b409e62c1be70a89c5f7f962aba46de10
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-10-16 12:46:44 +0100

    completed extension

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ function App() {
 		"id": 4,
 		"joke": "Chuck Norris does not own a stove, oven, or microwave, because revenge is a dish best served cold.",
 	}])
+	const filteredJokes = jokes.filter(joke => joke.id === 3);
 
 	return (
 		<div className="App">
@@ -39,6 +40,9 @@ function App() {
 
 			<h2>Jokes: </h2>
 			<ChuckJoke jokes={jokes} />
+
+			<h2>Filtered Jokes: </h2>
+			<ChuckJoke jokes={filteredJokes} />
 		</div>
 	);
 }
