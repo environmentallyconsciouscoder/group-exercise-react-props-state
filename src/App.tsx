ac89ef2be867ed885fca8d5337ddf8c57c900860
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-12-24 04:29:31 +0000

    refactor: using decontructor to tidy up the jokes array

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,30 @@ import ChuckJoke from './components/chuck_joke';
 import Joke from './joke';
 
 
+// ChuckJoke is being tasked with handling an array of jokes, which seems to be beyond the scope suggested by its name.
+// For the purposes of this exercise,
+// it'd be nominatively sensible to commit the cardinal sin of having App.tsx map/filter over the data to render a ChuckJoke
+// for each element of the array.
+
+// This refactor introduces some implications for how you approach the type within chuck_joke.tsx.
+
+// I'd suggest familiarising yourself with TS's utility types: https://www.typescriptlang.org/docs/handbook/utility-types.html
+// There are a whole bunch there, so to narrow your focus a little,
+// particularly useful/common utility types we see in React apps are Pick, Omit and Record:
+
+// E.g.
+// If indeed our ChuckJoke component were to only need the joke string, we can 'pick' the keys we want from the existing Joke type:
+
+// const ChuckJoke: React.FC< Pick<Joke, 'joke'> > = ( { joke } ) => ( <p> { joke } </p> )
+
+// Pretty cool, if you ask me.
+
 function App() {
 
-	const [chuckGreeting, setChuckGreeting] = useState<string>("I am Chuck Norris!!!")
-	const [whalesSaved, setWhalesSaved] = useState<number>(700)
-	const [roundHouseKicks, setRoundHouseKicks] = useState<number>(300000)
-	const [jokes, setJokes] = useState<Array<Joke>>([{
+	const [chuckGreeting, setChuckGreeting] = useState("I am Chuck Norris!!!")
+	const [whalesSaved, setWhalesSaved] = useState(700)
+	const [roundHouseKicks, setRoundHouseKicks] = useState(300000)
+	const [jokes, setJokes] = useState([{
 		"id": 1,
 		"joke": "Chuck Norris doesn’t read books. He stares them down until he gets the information he wants.",
 	},
@@ -39,10 +57,16 @@ function App() {
 			<ChuckInfo numberOfWhalesSaved={whalesSaved} numberOfRoundHouseKicks={roundHouseKicks} />
 
 			<h2>Jokes: </h2>
-			<ChuckJoke jokes={jokes} />
+			{/* <ChuckJoke jokes={jokes} /> */}
+			{jokes.map(({ id, joke }) => (
+				<ChuckJoke key={id} jokes={joke} />
+			))}
 
 			<h2>Filtered Jokes: </h2>
-			<ChuckJoke jokes={filteredJokes} />
+			{/* <ChuckJoke jokes={filteredJokes} /> */}
+			{filteredJokes.map(({ id, joke }) => (
+				<ChuckJoke key={id} jokes={joke} />
+			))}
 		</div>
 	);
 }
--- src/components/chuck_joke.tsx
+++ src/components/chuck_joke.tsx
@@ -1,18 +1,19 @@
+
 import Joke from "../joke";
 interface ChuckJokeProps {
-    jokes: Array<Joke>
+    // jokes: Array<Joke>
+    jokes: string
 }
+
 
 const ChuckJoke: React.FC<ChuckJokeProps> = (jokes) => {
     return (
-        <ul>
-        {jokes.jokes.map((joke) => <li key={joke.id}>
-            {joke.joke}</li>)}
-        </ul>
+        <>{jokes}</>
+        // <ul>
+        // {jokes.jokes.map((joke) => <li key={joke.id}>
+        //     {joke.joke}</li>)}
+        // </ul>
     )
 }
-
-
-
 
 export default ChuckJoke;
