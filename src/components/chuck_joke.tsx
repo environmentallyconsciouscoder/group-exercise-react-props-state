f841aecd63cbf280f8b30ac6a9bb449f0246d4d1
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-12-24 14:40:58 +0000

    refactor: removed unused codes

--- src/components/chuck_joke.tsx
+++ src/components/chuck_joke.tsx
@@ -1,18 +1,13 @@
 
 import Joke from "../joke";
 interface ChuckJokeProps {
-    // jokes: Array<Joke>
     jokes: string
 }
 
 
-const ChuckJoke: React.FC<ChuckJokeProps> = (jokes) => {
+const ChuckJoke: React.FC<ChuckJokeProps> = ({jokes}) => {
     return (
         <>{jokes}</>
-        // <ul>
-        // {jokes.jokes.map((joke) => <li key={joke.id}>
-        //     {joke.joke}</li>)}
-        // </ul>
     )
 }
 
